f9bf43aa3a1782cce023e8a5c419a2b3e9961d83
Kaiden Ong <kaiden.ong000@gmail.com>
2024-01-24 15:22:41 -0800

    Finished storied, need to run and add log photos

---
+++ app/src/main/java/edu/uw/ischool/kong314/activityspy/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.uw.ischool.kong314.activityspy
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val tag = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i(tag, "onCreate event fired, Parameter: $savedInstanceState")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i(tag, "onDestroy event fired")
+        Log.e(tag, "I'm the king of the world. - Titanic (1997)")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i(tag, "onPause event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i(tag, "onResume event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i(tag, "onStop event fired")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i(tag, "onStart event fired")
+    }
+}
